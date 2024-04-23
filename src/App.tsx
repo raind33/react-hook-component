cd1239af50516188a5f2e154b249d2bb4e383b2b
wuyuliang <2331336537@qq.com>
2024-04-23 19:24:16 +0800

    feat: 添加icon组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import dayjs from 'dayjs';
 import Calendar from './components/Calendar';
-
+import { IconAdd } from './components/Icon/IconAdd';
+import { createFromIconfont } from './components/Icon/createFrontIconfont';
+const IconFont = createFromIconfont('//at.alicdn.com/t/c/font_2321457_tjabignvdwn.js');
 function App() {
   return (
     <div className="App">
+       <IconFont type="r-shengdanbingqilin" spin size="40px"></IconFont>
+       <IconAdd spin size="40px"></IconAdd>
       <Calendar locale='en-US' className="a" value={dayjs('2024-10-23')}></Calendar>
     </div>
   );
