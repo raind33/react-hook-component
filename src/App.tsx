e19fce435513c047ac160f3fe0433e35554da36d
wuyuliang <2331336537@qq.com>
2024-05-20 18:00:30 +0800

    feat: 添加引导组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,5 @@
-import Popover from './components/Popover';
+import Demo from './components/OnBoarding/demo';
 
 export default function App() {
-
-  const popoverContent = <div>
-    光光光
-    <button onClick={() => {alert(1)}}>111</button>
-  </div>;
-
-  return <Popover
-    content={popoverContent}
-    placement='bottom'
-    trigger='click'
-    style={{margin: '200px'}}
-  >
-    <button>点我点我</button>
-  </Popover>
+  return <Demo/>
 }
---
+++ src/components/OnBoarding/Mask.tsx
@@ -0,0 +1,83 @@
+import React, { CSSProperties, useEffect, useState } from 'react';
+import { getMaskStyle } from './getMaskStyle'
+
+import './index.scss';
+
+interface MaskProps {
+  element: HTMLElement;
+
+  container?: HTMLElement;
+
+  renderMaskContent?: (wrapper: React.ReactNode) => React.ReactNode;
+
+  onAnimationStart?: () => void;
+
+  onAnimationEnd?: () => void;
+}
+
+export const Mask: React.FC<MaskProps> = (props) => {
+  const {
+    element,
+    renderMaskContent,
+    container,
+    onAnimationStart,
+    onAnimationEnd
+  } = props;
+
+  useEffect(() => {
+    onAnimationStart?.();
+    const timer = setTimeout(() => {
+      onAnimationEnd?.();
+    }, 200);
+
+    return () => {
+      window.clearTimeout(timer);
+    };
+  }, [element]);
+
+  const [style, setStyle] = useState<CSSProperties>({});
+
+  useEffect(() => {
+    const observer = new ResizeObserver(() => {
+      const style = getMaskStyle(element, container || document.documentElement);
+      setStyle(style);
+    });
+    observer.observe(container || document.documentElement);
+    return () => {
+      observer.disconnect()
+    }
+  }, [element]);
+
+  useEffect(() => {
+    if (!element) {
+      return;
+    }
+
+    element.scrollIntoView({
+        block: 'center',
+        inline: 'center'
+    });
+  
+    const style = getMaskStyle(element, container || document.documentElement);
+  
+    setStyle(style);
+    
+  }, [element, container]);
+
+  const getContent = () => {
+    if (!renderMaskContent) {
+      return null;
+    }
+    return renderMaskContent(
+      <div className={'mask-content'} style={{ width: '100%', height: '100%' }} ></div>
+    );
+  };
+
+  return (
+    <div
+      style={style}
+      className='mask'>
+      {getContent()}
+    </div>
+  );
+};
