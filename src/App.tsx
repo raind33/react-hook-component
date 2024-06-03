87065bd9c7f45d79c1dd9b481014c345265744c6
wuyuliang <2331336537@qq.com>
2024-06-03 16:45:32 +0800

    feat: 添加lazyload

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,48 @@
-import Demo from './components/Form/demo';
+// import Demo from './components/Form/demo';
+import React from 'react';
+import Lazyload from './components/Lazyload';
 
+const Demo = React.lazy(() => import('./components/Form/demo'))
 export default function App() {
-  return <Demo/>
+  return <div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div style={{height: '500px'}}>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<div>加了客人了</div>
+<Lazyload>
+<Demo/>
+
+</Lazyload>
+  </div>
 }
